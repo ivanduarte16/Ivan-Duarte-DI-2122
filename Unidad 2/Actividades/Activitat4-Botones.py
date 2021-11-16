0068aa30622e1df64ff46a216ac8ae4e8202e11f
Ivan Duarte <ivanduortiz2000@gmail.com>
2021-11-16 13:17:35 +0100

    Act5

--- Unidad 2/Actividades/Activitat4-Botones.py
+++ Unidad 2/Actividades/Activitat4-Botones.py
@@ -1,13 +1,13 @@
 from PySide6.QtCore import QSize
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-from config import ALTURA, ANCHO, btx, bty
+from config import *
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
-        self.setFixedSize(QSize(ALTURA, ANCHO))
+        self.setFixedSize(QSize(ALTURANOR, ANCHONOR))
         self.setWindowTitle("Exemple signals-slots 1")
 
         self.pybutton = QPushButton('Normalitza', self)
@@ -19,44 +19,32 @@ class MainWindow(QMainWindow):
         self.pybutton3.clicked.connect(self.minimitza)
 
         self.pybutton.resize(btx, bty)
-        self.pybutton.move((ALTURA - 100) / 2, (ANCHO - 50) / 2)
+        self.pybutton.move((ALTURANOR -100) / 2, (ANCHONOR - 50) / 2)
         self.pybutton2.resize(btx, bty)
-        self.pybutton2.move((ALTURA - 100) / 4.25, (ANCHO - 50) / 2)
+        self.pybutton2.move((ALTURANOR - 100) / 4.25, (ANCHONOR - 50) / 2)
         self.pybutton3.resize(btx, bty)
-        self.pybutton3.move((ALTURA - 100) / 1.3, (ANCHO - 50) / 2)
+        self.pybutton3.move((ALTURANOR - 100) / 1.3, (ANCHONOR - 50) / 2)
 
     def normalitza(self):
-        '''
-            S'executaà al rebre la notificació de que s'ha apretat el botó:
-            - Observeu que la consola imprimirà "Clic rebut!" al fer clic al botó
-        '''
         print('Clic rebut!, normalitza')
-        self.setFixedSize(QSize(ALTURA, ANCHO))
-        self.pybutton.move((ALTURA - 100) / 2, (ANCHO - 50) / 2)
-        self.pybutton2.move((ALTURA - 100) / 4.25, (ANCHO - 50) / 2)
-        self.pybutton3.move((ALTURA - 100) / 1.3, (ANCHO - 50) / 2)
+        self.setFixedSize(QSize(ALTURANOR, ANCHONOR))
+        self.pybutton.move((ALTURANOR - 100) / 2, (ANCHONOR- 50) / 2)
+        self.pybutton2.move((ALTURANOR - 100) / 4.25, (ANCHONOR - 50) / 2)
+        self.pybutton3.move((ALTURANOR - 100) / 1.3, (ANCHONOR - 50) / 2)
 
     def maximitza(self):
-        '''
-            S'executaà al rebre la notificació de que s'ha apretat el botó:
-            - Observeu que la consola imprimirà "Clic rebut!" al fer clic al botó
-        '''
         print('Clic rebut!, maximitza')
-        self.setFixedSize(QSize(ALTURA + 200, ANCHO + 200))
-        self.pybutton.move((ALTURA - 100) / 2, (ANCHO - 50) / 2)
-        self.pybutton2.move((ALTURA - 100) / 4.25, (ANCHO - 50) / 2)
-        self.pybutton3.move((ALTURA - 100) / 1.3, (ANCHO - 50) / 2)
+        self.setFixedSize(QSize(ALTURAMAX, ANCHOMAX))
+        self.pybutton.move((ALTURAMAX - 100) / 2, (ANCHOMAX - 50) / 2)
+        self.pybutton2.move((ALTURAMAX - 100) / 4.25, (ANCHOMAX - 50) / 2)
+        self.pybutton3.move((ALTURAMAX - 100) / 1.3, (ANCHOMAX - 50) / 2)
 
     def minimitza(self):
-        '''
-            S'executaà al rebre la notificació de que s'ha apretat el botó:
-            - Observeu que la consola imprimirà "Clic rebut!" al fer clic al botó
-        '''
         print('Clic rebut!, minimitza')
-        self.setFixedSize(QSize(ALTURA - 200, ANCHO - 200))
-        self.pybutton.move((ALTURA - 100) / 2, (ANCHO - 50) / 2)
-        self.pybutton2.move((ALTURA - 100) / 4.25, (ANCHO - 50) / 2)
-        self.pybutton3.move((ALTURA - 100) / 1.3, (ANCHO - 50) / 2)
+        self.setFixedSize(QSize(ALTURAMIN, ANCHOMIN))
+        self.pybutton.move((ALTURAMIN - 100) / 2, (ANCHOMIN - 50) / 2)
+        self.pybutton2.move((ALTURAMIN - 100) / 4.25, (ANCHOMIN - 50) / 2)
+        self.pybutton3.move((ALTURAMIN - 100) / 1.25, (ANCHOMIN - 50) / 2)
 
 
 if __name__ == "__main__":
